0e0f4207af90339ddf1700983af045eedc14e9b4
jarrenpatao <jarrenpatao@gmail.com>
2018-12-03 21:50:51 -0800

    computer is going to die. changed some stuff. check the notes.

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -2,3 +2,22 @@ $('#dropdownId').click(function(){
   $('#mainBody').show(true);
   $('#mainBody').addClass('animated fadeInUp');
 })
+
+$('#link1').click(function(){
+  $('#mainBody').html('<img src="assets/images/BPSS.png" style="margin-top: 20px" width="100%">')
+})
+$('#link2').click(function(){
+  $('#mainBody').html('<img src="assets/images/IMAGEHERE.JPG" style="margin-top: 20px" width="100%">')
+})
+$('#link3').click(function(){
+  $('#mainBody').html('<img src="assets/images/IMAGEHERE.JPG" style="margin-top: 20px" width="100%">')
+})
+$('#link4').click(function(){
+  $('#mainBody').html('<img src="assets/images/IMAGEHERE.JPG" style="margin-top: 20px" width="100%">')
+})
+$('#link5').click(function(){
+  $('#mainBody').html('<img src="assets/images/IMAGEHERE.JPG" style="margin-top: 20px" width="100%">')
+})
+$('#link6').click(function(){
+  $('#mainBody').html('<img src="assets/images/IMAGEHERE.JPG" style="margin-top: 20px" width="100%">')
+})
