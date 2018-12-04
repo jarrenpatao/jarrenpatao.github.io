47e768197bfd3a9e653276d98983a55aa02d9e78
jarrenpatao <jarrenpatao@gmail.com>
2018-12-03 22:29:35 -0800

    changed the dynamic fill of #mainBody, but will work to animate transition between projects.

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -4,20 +4,20 @@ $('#dropdownId').click(function(){
 })
 
 $('#link1').click(function(){
-  $('#mainBody').html('<img src="assets/images/BPSS.png" style="margin-top: 20px" width="100%">')
+  $('#mainBody').html('<img src="assets/images/screenshots/BPSS.png" style="margin-top: 20px" width="100%">')
 })
 $('#link2').click(function(){
-  $('#mainBody').html('<img src="assets/images/PGSS.png" style="margin-top: 20px" width="100%">')
+  $('#mainBody').html('<img src="assets/images/screenshots/PGSS.png" style="margin-top: 20px" width="100%">')
 })
 $('#link3').click(function(){
-  $('#mainBody').html('<img src="assets/images/CCSS.png" style="margin-top: 20px" width="100%">')
+  $('#mainBody').html('<img src="assets/images/screenshots/CCSS.png" style="margin-top: 20px" width="100%">')
 })
 $('#link4').click(function(){
-  $('#mainBody').html('<img src="assets/images/TGSS.png" style="margin-top: 20px" width="100%">')
+  $('#mainBody').html('<img src="assets/images/screenshots/GTSS.png" style="margin-top: 20px" width="100%">')
 })
 $('#link5').click(function(){
-  $('#mainBody').html('<img src="assets/images/TSSS.png" style="margin-top: 20px" width="100%">')
+  $('#mainBody').html('<img src="assets/images/screenshots/TSSS.png" style="margin-top: 20px" width="100%">')
 })
 $('#link6').click(function(){
-  $('#mainBody').html('<img src="assets/images/POSS.png" style="margin-top: 20px" width="100%">')
+  $('#mainBody').html('<img src="assets/images/screenshots/POSS.png" style="margin-top: 20px" width="100%">')
 })
