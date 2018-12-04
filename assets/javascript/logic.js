d9bbb4e6fe19ed60d23e4e4e2602e67b09a74c0d
jarrenpatao <jarrenpatao@gmail.com>
2018-12-03 22:10:38 -0800

    added images which will link to each project repository.

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -7,17 +7,17 @@ $('#link1').click(function(){
   $('#mainBody').html('<img src="assets/images/BPSS.png" style="margin-top: 20px" width="100%">')
 })
 $('#link2').click(function(){
-  $('#mainBody').html('<img src="assets/images/IMAGEHERE.JPG" style="margin-top: 20px" width="100%">')
+  $('#mainBody').html('<img src="assets/images/PGSS.png" style="margin-top: 20px" width="100%">')
 })
 $('#link3').click(function(){
-  $('#mainBody').html('<img src="assets/images/IMAGEHERE.JPG" style="margin-top: 20px" width="100%">')
+  $('#mainBody').html('<img src="assets/images/CCSS.png" style="margin-top: 20px" width="100%">')
 })
 $('#link4').click(function(){
-  $('#mainBody').html('<img src="assets/images/IMAGEHERE.JPG" style="margin-top: 20px" width="100%">')
+  $('#mainBody').html('<img src="assets/images/TGSS.png" style="margin-top: 20px" width="100%">')
 })
 $('#link5').click(function(){
-  $('#mainBody').html('<img src="assets/images/IMAGEHERE.JPG" style="margin-top: 20px" width="100%">')
+  $('#mainBody').html('<img src="assets/images/TSSS.png" style="margin-top: 20px" width="100%">')
 })
 $('#link6').click(function(){
-  $('#mainBody').html('<img src="assets/images/IMAGEHERE.JPG" style="margin-top: 20px" width="100%">')
+  $('#mainBody').html('<img src="assets/images/POSS.png" style="margin-top: 20px" width="100%">')
 })
