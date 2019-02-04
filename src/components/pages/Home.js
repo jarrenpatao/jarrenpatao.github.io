cf578345b65966a1fd7af50d4aa7001ce71ed918
jarrenpatao <jarrenpatao@gmail.com>
2019-02-04 09:46:53 -0800

    moved parallax to top

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -10,10 +10,20 @@ function Home() {
     
     <div>
     <hr />
+    <Parallax
+      // blur={5}
+      bgImage={require('../../assets/images/myLogoWide.png')}
+      bgImageAlt="Haiku Mill | Maui, HI"
+      strength={200}
+      borderRadius={20}
+      className="logoLimit">
+    <div className="paraBlock"></div>
+    </Parallax>
+    <br />
     <MDBContainer>
       <MDBRow>
-        <MDBCol sm="2"></MDBCol>
-        <MDBCol sm="8">
+        <MDBCol sm="1"></MDBCol>
+        <MDBCol sm="9">
           <img className="profPic" alt="Cheers!" src="http://i103.photobucket.com/albums/m130/minibalogna00/6W7Y9721.jpg" />
           <p className="aboutMe">
             Having moved from Hawaii, I've come to see the amazing world of tech that lives here in the Bay area. My passion lives in full stack with a focus on UX/UI. The user experience is what our community sees and feels, but the functionality is what drives us.
@@ -33,20 +43,12 @@ function Home() {
           </div>
         </MDBCol>
       </MDBRow>
-      <MDBCol sm="2"></MDBCol>
+      <MDBCol sm="1"></MDBCol>
       </MDBContainer>
       <MDBRow>
       <MDBCol>
         <br />
-        <Parallax
-          // blur={5}
-          bgImage={require('../../assets/images/myLogo.png')}
-          bgImageAlt="Haiku Mill | Maui, HI"
-          strength={200}
-          borderRadius={20}>
-          <div className="paraBlock"></div>
-        </Parallax>
-        <br />
+        
           {/* <p>Lorem ipsum dolor amet 8-bit squid everyday carry iPhone. Vice jianbing marfa synth fam asymmetrical vaporware copper mug food truck keffiyeh wolf photo booth literally seitan. Tofu banh mi hashtag fanny pack listicle. Umami before they sold out affogato prism four loko letterpress disrupt flexitarian health goth biodiesel gastropub cronut. Vape ethical humblebrag yuccie cornhole wolf 90's. Banjo pug VHS asymmetrical beard messenger bag marfa knausgaard freegan tousled pickled butcher.
           <br /><br />
           Lyft deep v iPhone biodiesel organic trust fund, letterpress skateboard butcher post-ironic mustache. Knausgaard XOXO put a bird on it street art mlkshk biodiesel. Fanny pack man braid thundercats keffiyeh hexagon, sriracha portland before they sold out cred. Squid tote bag enamel pin salvia cliche hell of, crucifix af forage gluten-free narwhal. Oh. You need a little dummy text for your mockup? How quaint. I bet you’re still using Bootstrap too…</p> */}
